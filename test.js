050896553e189f53b3463d9b5d262364e494b3b2
saphire melody <96967601+saphiremelody@users.noreply.github.com>
2023-01-26 17:44:39 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,17 @@
+const discord = require ("discord.js");
+
+
+
+
+module.exports.run = async (bot, message, args) => {
+
+  
+                 message.channel.send('bot is working.');
+               }
+             }
+  
+  
+
+module.exports.help = {
+  name:"test"
+}
