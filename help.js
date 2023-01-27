1fd1f514b5d867095bba7b06e881e7364e4a4705
saphire melody <96967601+saphiremelody@users.noreply.github.com>
2023-01-27 18:08:05 -0500

    Delete help.js

--- help.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-               message.channel.send('u!help, u!test, u!ping, u!userinfo. u!lockdown on/off, u!ban, u!backup, u!backupinfo, u!restore');
-}
-
-module.exports.help = {
-  name:"help"
-}
